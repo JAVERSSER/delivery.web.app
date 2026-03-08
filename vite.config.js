a41b6b5c8a6134b8bc9e22f6a4054f5ffd34db66
Thirith Heng <thirithheng@gmail.com>
2026-03-08 11:20:41 +0700

    Init

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,16 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom'],
-          firebase: ['firebase/app', 'firebase/auth', 'firebase/firestore'],
-        },
-      },
-    },
-    // Increase the warning limit if you are okay with bigger chunks
-    chunkSizeWarningLimit: 1000, // in KB
+  server: { host: true, port: 5175 },
+  optimizeDeps: {
+    include: [
+      'firebase/app',
+      'firebase/auth',
+      'firebase/firestore',
+      'firebase/database',
+      'firebase/storage',
+    ],
   },
 })
